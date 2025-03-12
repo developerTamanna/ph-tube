a893abb00d026f8167910d7e82ac5f5c9ddec50b
developerTamanna <astamanna57@gmail.com>
2025-03-13 00:02:38 +0600

    active button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+function removeActive(){
+    const activeBtns = document.getElementsByClassName("active")
+    // console.log(activeBtns)
+    for(let btn of activeBtns){
+        btn.classList.remove("active");
+    }
+}
+
 function loadCategories() {
     
     //* fetch the data
@@ -22,7 +30,23 @@ function loadCategories() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((response) =>response.json() )
     .then((data) => {
+        document.getElementById("btn-all").classList.add.add("active")
         (displayVideos(data.videos))
+    } )
+   }
+
+   const loadCategoryVideos =(id)=>{
+    // console.log(id)
+    const url =`https://openapi.programming-hero.com/api/phero-tube/category/${id}`
+    console.log(url)
+    fetch(url)
+    .then((res) =>res.json() )
+    .then((data) =>{
+        removeActive()
+        const clickButton =document.getElementById(`btn-${id}`);
+        clickButton.classList.add("active")
+        console.log(clickButton)
+        displayVideos(data.category)
     } )
    }
 // category dekhanor function
@@ -38,18 +62,31 @@ function loadCategories() {
              const categoryDiv = document.createElement("div")
             //*append the element
             categoryDiv.innerHTML = `
-               <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+               <button id="btn-${cat.category_id}" onclick ="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
             `
             categoriesContainer.append(categoryDiv)
         }
     }
 
  //{category_id: '1001', video_id: 'aaal', thumbnail: 'https://i.ibb.co/hdtZYbB/enchnting.jpg', title: 'Enchanted Harmonies', authors: Array(1), …}
+
+
  
 const displayVideos=(videos)=>{
 //    console.log(videos)
 const videoContainer = document.getElementById(" video-container");
+ videoContainer.innerHTML ="";
+ if(videos.length ==0){
+    videoContainer.innerHTML =`
+    <div class="py-20 col-span-full flex flex-col text-center justify-center items-center">
+  <img class="w-[120px] " src="./assest/Icon.png" alt="">
+  <h2> videoContainer.innerHTML =""</h2>
+</div>
+    `
+ return ;
+ }
 videos.forEach(video => {
+   
     console.log(video)
     const videoCard = document.createElement("div")
     videoCard.innerHTML =`
